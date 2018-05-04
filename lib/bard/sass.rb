fb4850f9377ef1efb7077944b061d6b446ede763
Micah Geisel <micah@botandrose.com>
2018-05-03 18:59:13 -0700

    steal font-face mixin from bourbon.

--- lib/bard/sass.rb
+++ lib/bard/sass.rb
@@ -1,7 +1,17 @@
+require "sass"
 require "bard/sass/version"
 
 module Bard
   module Sass
-    # Your code goes here...
+    SASS_PATH = File.expand_path("../../sass", __dir__)
+
+    if defined?(Rails) && defined?(Rails::Engine)
+      class Engine < Rails::Engine
+        config.assets.paths << SASS_PATH
+      end
+    else
+      Sass.load_paths << SASS_PATH
+    end
   end
 end
+
