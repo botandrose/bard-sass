835d7895ca6f848e92de831a72b74872a995ccf1
Micah Geisel <micah@botandrose.com>
2018-12-17 16:33:51 -0700

    drop support for sass and sassc 1.x.

--- lib/bard/sass.rb
+++ lib/bard/sass.rb
@@ -1,16 +1,11 @@
-require "sass"
 require "bard/sass/version"
 
 module Bard
   module Sass
-    SASS_PATH = File.expand_path("../../sass", __dir__)
-
     if defined?(Rails) && defined?(Rails::Engine)
       class Engine < Rails::Engine
-        config.assets.paths << SASS_PATH
+        config.assets.paths << File.expand_path("../../sass", __dir__)
       end
-    else
-      Sass.load_paths << SASS_PATH
     end
   end
 end
