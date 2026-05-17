cffe12dc78be42f6c583f316b2e9a5b794696cbe
Micah Geisel <micah@botandrose.com>
2026-05-17 12:08:57 +0100

    require sprockets when needed.

--- lib/bard/sass.rb
+++ lib/bard/sass.rb
@@ -3,6 +3,7 @@ require "bard/sass/version"
 module Bard
   module Sass
     if defined?(Rails) && defined?(Rails::Engine)
+      require "sprockets/railtie"
       class Engine < Rails::Engine
         config.assets.paths << File.expand_path("../../sass", __dir__)
       end
